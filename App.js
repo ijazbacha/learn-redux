799adfefa56d91fc9d42510500bb76c8ab5d0f27
bacha-11 <ijazbacha14@gmail.com>
2021-10-23 14:35:31 +0500

    Create store and reducer

--- App.js
+++ App.js
@@ -2,13 +2,17 @@ import React from 'react';
 import { createStore } from 'redux';
 import { StyleSheet, Text, View } from 'react-native';
 import Counter from './src/Screens/Counter';
+import RootReducer from './src/Store/Reducer';
+import { Provider } from 'react-redux';
 
+const store = createStore(RootReducer)
 
 export default function App() {
   return (
     <View style={styles.container}>
-    
+    <Provider store={store}>
       <Counter />
+    </Provider>
     </View>
   );
 }
---
+++ src/Store/Reducer.js
@@ -0,0 +1,9 @@
+const initialState = {
+    counter: 0
+}
+
+const RootReducer = (state = initialState, action) => {
+    return state
+}
+
+export default RootReducer;
